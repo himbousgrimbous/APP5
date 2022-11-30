7b131457aea2c78dc5fd27c00fa047e1ce763cd8
ALK02 <79812201+ALK02@users.noreply.github.com>
2022-11-30 12:02:11 +0100

    Update socialNumberException.java

--- socialNumberException.java
+++ socialNumberException.java
@@ -1,9 +1,10 @@
 package model;
+import javax.swing.JOptionPane;
 
 public class socialNumberException extends Exception{
 	
 	public socialNumberException(String errorMessage) {
 		//call constructer
-		JOptionPane.showMessageDialog(null, "Error : The customer security number must be more than 13 characters")
+		JOptionPane.showMessageDialog(null, "Error : The customer security number must be more than 13 characters");
 	}
 }
