54d1b3616fb37cba27b198b39826b871f1adc352
ComeMeyer <103130814+ComeMeyer@users.noreply.github.com>
2022-12-02 15:24:52 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,6 +2,13 @@ package model;
 
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 public class Client {
 
 
@@ -10,13 +17,15 @@ public class Client {
 	    public String socialNb;
 	    public String phoneNb;
 	    public String address;
-	    public ArrayList<Drug> medicineList;
+	    public float totalPrice;
+	    public ArrayList<BoughtDrug> medicineList;
 	    public String clientDisplayInfos() {
         	return ("Client's social number: "+ this.socialNb +
         		"\nClient's name: "+ this.lastName+ " "+this.firstName+
                 "\nClient's phone number: "+this.phoneNb+
                 "\nClient's address: "+this.address+
-                "\nClient's purchases: "+this.medicineList);
+                "\nClient's total bill price: "+this.totalPrice+ " €"+
+                "\nClient's drug(s) bought: ");
    		}
 
 		public Client(String socialNb, String firstName, String lastName, String phoneNb, String address) throws Exception {
@@ -28,9 +37,83 @@ public class Client {
 	        this.socialNb = socialNb;
 	        this.phoneNb = phoneNb;
 	        this.address = address;
-	        this.medicineList = new ArrayList<Drug>();
+	        this.medicineList = new ArrayList<BoughtDrug>();
 	    }
-	    public void addDrug(Drug medicine) {
+		
+		public static String buildClientDrugPanel(Client client) {
+			String textToDisplay = new String();
+			
+			if (!client.getMedicineList().isEmpty()) {
+			for(BoughtDrug d : client.getMedicineList()) {
+				textToDisplay += "\n- " + d.getWording() + ": " + d.getQuantity()+" units";
+			}
+			return textToDisplay;
+		}
+		return null;
+	}
+		
+		public static void addTotalPrice(Client client, float priceToAdd) {
+			client.setTotalPrice(client.getTotalPrice() + priceToAdd);;
+		}
+		
+	    public void addDrug(BoughtDrug medicine) {
 	        medicineList.add(medicine);
-	    }    
+	    }
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getSocialNb() {
+			return socialNb;
+		}
+
+		public void setSocialNb(String socialNb) {
+			this.socialNb = socialNb;
+		}
+
+		public String getPhoneNb() {
+			return phoneNb;
+		}
+
+		public void setPhoneNb(String phoneNb) {
+			this.phoneNb = phoneNb;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public ArrayList<BoughtDrug> getMedicineList() {
+			return medicineList;
+		}
+
+		public void setMedicineList(ArrayList<BoughtDrug> medicineList) {
+			this.medicineList = medicineList;
+		}
+
+		public float getTotalPrice() {
+			return totalPrice;
+		}
+
+		public void setTotalPrice(float totalPrice) {
+			this.totalPrice = totalPrice;
+		}
+  
 }
